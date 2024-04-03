1ed0a97cda54d05668a0779df73e2b88f6ba195e
Malyuk-A <malyuk.alexander1999@gmail.com>
2024-04-03 14:36:30 +0200

    Add comment

--- oak_cli/main.py
+++ oak_cli/main.py
@@ -7,6 +7,11 @@ from oak_cli.utils.argcomplete import handle_argcomplete
 
 def main():
     handle_argcomplete()
+    # Potential Future Work:
+    # This implementation uses argparse & argcomplete for user friendly CLI behavior.
+    # However especially argparse comes with a lot of additional boilerplate code.
+    # It might be useful to look into more modern solutions for python CLI's like
+    # https://github.com/pallets/click
     parse_arguments_and_execute()
 
 
