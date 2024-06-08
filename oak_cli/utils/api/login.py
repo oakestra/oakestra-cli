100024a8f37fc387dd22d8e8c5010fdd893c445b
Malyuk-A <malyuk.alexander1999@gmail.com>
2024-06-08 15:47:07 +0200

    Improve login

--- oak_cli/utils/api/login.py
+++ oak_cli/utils/api/login.py
@@ -1,9 +1,13 @@
+from datetime import datetime
+
 import oak_cli.utils.api.custom_requests as custom_requests
 from oak_cli.utils.api.common import SYSTEM_MANAGER_URL
 from oak_cli.utils.api.custom_http import HttpMethod
 from oak_cli.utils.exceptions.types import OakCLIExceptionTypes
 
 _login_token = ""
+# NOTE: The token will become unusable after some time.
+_last_login_time = None
 
 
 class LoginFailed(Exception):
@@ -27,10 +31,16 @@ def _login_and_set_token() -> str:
 
     global _login_token
     _login_token = response["token"]
+    global _last_login_time
+    _last_login_time = datetime.now()
     return _login_token
 
 
 def get_login_token() -> str:
-    if _login_token == "":
+    if (
+        _login_token == ""
+        or _last_login_time is None
+        or (datetime.now() - _last_login_time).total_seconds() > 10
+    ):
         return _login_and_set_token()
     return _login_token
