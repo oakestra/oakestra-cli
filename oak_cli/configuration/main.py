a4999c80e9944639dd15147608f00ecb19b4c2e1
Malyuk-A <malyuk.alexander1999@gmail.com>
2024-06-22 14:53:48 +0200

    Started adding more flops support cmds

--- oak_cli/configuration/main.py
+++ oak_cli/configuration/main.py
@@ -2,6 +2,7 @@ import pprint
 
 import typer
 
+import oak_cli.configuration.addon_repos
 import oak_cli.configuration.local_machine_purpose
 import oak_cli.configuration.main_oak_repo
 import oak_cli.docker.cluster_orchestrator
@@ -11,6 +12,11 @@ from oak_cli.utils.typer_augmentations import AliasGroup
 
 app = typer.Typer(cls=AliasGroup)
 
+app.add_typer(
+    typer_instance=oak_cli.configuration.local_machine_purpose.app,
+    name="local-machine-purpose",
+    help="Manage the local machine purpose w.r.t. Oakestra",
+)
 
 app.add_typer(
     typer_instance=oak_cli.configuration.main_oak_repo.app,
@@ -18,9 +24,9 @@ app.add_typer(
     help="Configure the main Oakestra repository",
 )
 app.add_typer(
-    typer_instance=oak_cli.configuration.local_machine_purpose.app,
-    name="local-machine-purpose",
-    help="Manage the local machine purpose w.r.t. Oakestra",
+    typer_instance=oak_cli.configuration.addon_repos.app,
+    name="addon-repos",
+    help="Configure addon repositories",
 )
 
 
