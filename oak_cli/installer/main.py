99ca5f6d8a4c39358dd349c178e453467d1c19fe
Malyuk-A <malyuk.alexander1999@gmail.com>
2024-06-26 18:03:07 +0200

    Added go install role to install cmd

--- oak_cli/installer/main.py
+++ oak_cli/installer/main.py
@@ -4,6 +4,8 @@ import typer
 from oak_cli.ansible.python_utils import CliPlaybook
 from oak_cli.utils.common import run_in_shell
 
+ANSIBLE_GALAXY_ROLES = " ".join(("geerlingguy.docker", "gantsign.golang"))
+
 app = typer.Typer()
 
 
@@ -15,7 +17,7 @@ app = typer.Typer()
     """,
 )
 def install_fundamentals() -> None:
-    # NOTE: The following playbook requires a ansible-galaxy role to be installed on the machine.
+    # NOTE: The following playbook requires ansible-galaxy roles to be installed on the machine.
     # Installing it via a dedicated playbook does not work due to ansible-access right issues.
-    run_in_shell(shell_cmd="ansible-galaxy install geerlingguy.docker")
+    run_in_shell(shell_cmd=f"ansible-galaxy install {ANSIBLE_GALAXY_ROLES}")
     ansible_runner.run(playbook=CliPlaybook.INSTALL_FUNDAMENTALS.get_path())
