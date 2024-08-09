1660fa08f7f3d5c3742db02b3d80c897a181d355
Malyuk-A <malyuk.alexander1999@gmail.com>
2024-08-09 19:55:01 +0200

    Hierarchical graph logic mostly done

--- oak_cli/evaluation/addons/flops/utils/special_graphs/line_graphs.py
+++ oak_cli/evaluation/addons/flops/utils/special_graphs/line_graphs.py
@@ -7,6 +7,8 @@ from oak_cli.evaluation.addons.flops.utils.keys import (
     DISK_START_KEY,
     MEMORY_KEY,
     NETWORK_START_KEYS,
+    NETWORK_START_RECEIVED_KEY,
+    NETWORK_START_SENT_KEY,
     NODE_KEY,
     RUN_ID_KEY,
     STAGE_KEY,
@@ -79,13 +81,27 @@ def draw_memory_linegraph(
     )
 
 
-def draw_disk_space_linegraph(normalized_data: pd.DataFrame) -> None:
+def draw_disk_space_linegraph(
+    normalized_data: pd.DataFrame,
+    multi_cluster: bool = False,
+) -> None:
+    data_keys = [DISK_START_KEY, STAGE_KEY]
+    if multi_cluster:
+        data_keys.append(NODE_KEY)
+
     _normalized_df = normalized_data.copy()
     _normalized_df[[DISK_START_KEY]] = _normalized_df[[DISK_START_KEY]] / 1024
     draw_graph(
         title="Evaluation Runs Average",
-        data=_normalized_df[[DISK_START_KEY, STAGE_KEY, RUN_ID_KEY]],
-        plot_functions=[lambda: sns.lineplot(data=_normalized_df[[DISK_START_KEY, STAGE_KEY]])],
+        data=_normalized_df[data_keys + [RUN_ID_KEY]],
+        plot_functions=[
+            lambda: sns.lineplot(
+                x=TIME_START_KEY,
+                y=DISK_START_KEY,
+                data=_normalized_df[data_keys],
+                hue=NODE_KEY if multi_cluster else None,
+            )
+        ],
         y_label="Disk Space Change (GB)",
         x_lim=(0, max(_normalized_df.index)),
         y_lim=0,
@@ -104,6 +120,66 @@ def draw_network_linegraph(normalized_data: pd.DataFrame) -> None:
             lambda: sns.lineplot(data=_normalized_df[NETWORK_START_KEYS + [STAGE_KEY]])
         ],
         y_label="Network Change (I/O) (GB)",
+        x_lim=(0, max(_normalized_df.index)),
+        y_lim=0,
+        show_stages=True,
+        use_median_stages=True,
+    )
+
+
+def draw_network_received_linegraph(
+    normalized_data: pd.DataFrame,
+    multi_cluster: bool = False,
+) -> None:
+    data_keys = [NETWORK_START_RECEIVED_KEY, STAGE_KEY]
+    if multi_cluster:
+        data_keys.append(NODE_KEY)
+
+    _normalized_df = normalized_data.copy()
+    _normalized_df[[NETWORK_START_RECEIVED_KEY]] = (
+        _normalized_df[[NETWORK_START_RECEIVED_KEY]] / 1024
+    )
+    draw_graph(
+        title="Evaluation Runs Average",
+        data=_normalized_df[data_keys + [RUN_ID_KEY]],
+        plot_functions=[
+            lambda: sns.lineplot(
+                x=TIME_START_KEY,
+                y=NETWORK_START_RECEIVED_KEY,
+                data=_normalized_df[data_keys],
+                hue=NODE_KEY if multi_cluster else None,
+            )
+        ],
+        y_label="Network Received (GB)",
+        x_lim=(0, max(_normalized_df.index)),
+        y_lim=0,
+        show_stages=True,
+        use_median_stages=True,
+    )
+
+
+def draw_network_send_linegraph(
+    normalized_data: pd.DataFrame,
+    multi_cluster: bool = False,
+) -> None:
+    data_keys = [NETWORK_START_SENT_KEY, STAGE_KEY]
+    if multi_cluster:
+        data_keys.append(NODE_KEY)
+
+    _normalized_df = normalized_data.copy()
+    _normalized_df[[NETWORK_START_SENT_KEY]] = _normalized_df[[NETWORK_START_SENT_KEY]] / 1024
+    draw_graph(
+        title="Evaluation Runs Average",
+        data=_normalized_df[data_keys + [RUN_ID_KEY]],
+        plot_functions=[
+            lambda: sns.lineplot(
+                x=TIME_START_KEY,
+                y=NETWORK_START_SENT_KEY,
+                data=_normalized_df[data_keys],
+                hue=NODE_KEY if multi_cluster else None,
+            )
+        ],
+        y_label="Network Sent (GB)",
         x_lim=(0, max(_normalized_df.index)),
         y_lim=0,
         show_stages=True,
