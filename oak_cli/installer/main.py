e234b4abd59d1b68b68b6ca0e11def2da17ec49b
Malyuk-A <malyuk.alexander1999@gmail.com>
2025-01-29 12:54:08 +0000

    Fix dependency installation and migrate to ruff

--- oak_cli/installer/main.py
+++ oak_cli/installer/main.py
@@ -1,8 +1,12 @@
 import ansible_runner
 import typer
-
+import getpass
 from oak_cli.ansible.python_utils import CliPlaybook
 from oak_cli.utils.common import run_in_shell
+from oak_cli.utils.styling import create_spinner
+from contextlib import nullcontext
+from oak_cli.utils.logging import logger
+import sys
 
 ANSIBLE_GALAXY_ROLES = " ".join(("geerlingguy.docker", "gantsign.golang"))
 
@@ -16,8 +20,24 @@ app = typer.Typer()
     on the current machine
     """,
 )
-def install_fundamentals() -> None:
+def install_fundamentals(
+    show_ansible_output: bool = typer.Option(False, "--verbose"),
+    ) -> None:
     # NOTE: The following playbook requires ansible-galaxy roles to be installed on the machine.
     # Installing it via a dedicated playbook does not work due to ansible-access right issues.
     run_in_shell(shell_cmd=f"ansible-galaxy install {ANSIBLE_GALAXY_ROLES}")
-    ansible_runner.run(playbook=CliPlaybook.INSTALL_FUNDAMENTALS.get_path())
+    become_password = getpass.getpass("[sudo] password: ")
+    spinner_context = (
+        create_spinner(message="Installing Dependencies") if not show_ansible_output else nullcontext()
+    )
+    with spinner_context:
+        res = ansible_runner.run(
+            playbook=CliPlaybook.INSTALL_FUNDAMENTALS.get_path(),
+            extravars={"ansible_become_pass": become_password},
+            quiet=not show_ansible_output,
+        )
+    if res.rc != 0:
+        logger.fatal(
+            "Dependency installation failed! Are you certain that you provided the correct password?"
+        )
+        sys.exit(1)
